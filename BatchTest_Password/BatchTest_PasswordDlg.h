5bd2a416c78e4c7c18d95b4bd9db00f0a5fe72d3
guan <1045248469@qq.com>
2020-06-21 23:11:28 +0800

    添加项目文件。

---
+++ BatchTest_Password/BatchTest_PasswordDlg.h
@@ -0,0 +1,37 @@
+﻿
+// BatchTest_PasswordDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CBatchTestPasswordDlg 对话框
+class CBatchTestPasswordDlg : public CDialogEx
+{
+// 构造
+public:
+	CBatchTestPasswordDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_BATCHTEST_PASSWORD_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_password;//edit控件
+	afx_msg void OnBnClickedButtonPw();//点击生成密码按钮
+};
